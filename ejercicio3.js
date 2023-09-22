a68d61fb6267ae59cd686a1b6dda16f018c9bad0
Carlos Terrero <carlosterrero2@gmail.com>
2023-09-22 19:57:15 +0200

    ejercicio 3 de JavaScript

---
+++ ejercicio3.js
@@ -0,0 +1,16 @@
+var saludo = "Hola";
+let verdadero = true;
+const numero = 3;
+
+console.log(saludo);
+console.log(typeof saludo);
+console.log(verdadero);
+console.log(typeof verdadero);
+console.log(numero);
+console.log(typeof numero);
+
+function local(name){
+    return local = ("Soy global")
+}
+
+local();
