724b4d519d51a1c0381f0179d5e3b1edd316f067
Carlos Terrero <carlosterrero2@gmail.com>
2023-09-27 19:57:05 +0200

    ejercicios java iconos

--- ponerquitariconos.js
+++ ponerquitariconos.js
@@ -1,25 +1,36 @@
 let array = [];
 let arrayIconos = ["🎴", "🃏", "🥵", "🌶️", "🍓", "🥛", "🍄", "🍋", "🍍", "🍕", "🍺", "🥑"];
 
-    function displayArray(){
-        document.getElementById("array").textContent = array.join(", ");
-    }
-        document.getElementById("push").addEventListener("click", function(){
-        array.push(arrayIconos[Math.floor(Math.random()*arrayIconos.length)]);
-        displayArray();        
+function getRandomItem(){
+	return arrayIconos[Math.floor(Math.random() * arrayIconos.length)];
+}
+
+function displayArray() {
+	document.getElementById("array").textContent = array.join(", ");
+}
+document.getElementById("push").addEventListener("click", () => {
+	array.push(getRandomItem());
+	displayArray();
 });
 
-        document.getElementById("unshift").addEventListener("click", function(){
-        array.unshift(arrayIconos[Math.floor(Math.random()*arrayIconos.length)]);
-        displayArray();
+document.getElementById("unshift").addEventListener("click", () => {
+	array.unshift(getRandomItem());
+	displayArray();
 });
 
-        document.getElementById("pop").addEventListener("click", function(){
-        array.pop(arrayIconos[Math.floor(Math.random()*arrayIconos.length)]);
-        displayArray();
+document.getElementById("pop").addEventListener("click", () => {
+	array.pop(getRandomItem());
+	displayArray();
 });
 
-        document.getElementById("shift").addEventListener("click", function(){
-        array.shift(arrayIconos[Math.floor(Math.random()*arrayIconos.length)]);
-        displayArray();
+document.getElementById("shift").addEventListener("click", () => {
+	array.shift(getRandomItem());
+	displayArray();
+});
+
+document.getElementById("input1").addEventListener("click", () => {
+	const position = document.getElementById("input1").value;
+	array.splice(position, 1);
+	displayArray();
+
 });
