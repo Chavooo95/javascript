16d5fc05411e32cf053eecd25a196feca57f494a
Carlos Terrero <carlosterrero2@gmail.com>
2023-09-28 17:05:18 +0200

    empiezo piedra papel tijera

---
+++ ppt.js
@@ -0,0 +1,14 @@
+let array = []
+let arrayPPT = ["✊", "✋", "✌️"]
+
+function getRandomItem(){
+	return arrayPPT[Math.floor(Math.random() * arrayPPT.length)];
+}
+
+const btnpiedra = document.getElementById("piedra");
+const btnpapel = document.getElementById("papel");
+const btntijera = document.getElementById("tijera");
+
+function escribirTexto(){
+    
+}
