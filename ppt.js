7f90da64a4f51d04e2ca7f4af39f990d32095b4e
Carlos Terrero <carlosterrero2@gmail.com>
2023-09-28 17:36:10 +0200

    sigo con piedra papel tijera

--- ppt.js
+++ ppt.js
@@ -1,14 +1,21 @@
-let array = []
-let arrayPPT = ["✊", "✋", "✌️"]
+let array = [];
+let arrayPPT = ["✊", "✋", "✌️"];
+
+function displayArray() {
+	document.getElementById("array").textContent = array.join("Otra vez será chavalote");
+};
 
 function getRandomItem(){
 	return arrayPPT[Math.floor(Math.random() * arrayPPT.length)];
-}
+};
 
 const btnpiedra = document.getElementById("piedra");
 const btnpapel = document.getElementById("papel");
 const btntijera = document.getElementById("tijera");
 
 function escribirTexto(){
-    
-}
+    document.getElementById("boton").addEventListener("click", () =>{
+        displayArray();
+
+    })
+};
